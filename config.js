f900a801d8dd4001e9bbfa32cc01a8e0af5b54b9
Andrey Sarapulov <sarapulov@Andreys-MBP.fritz.box>
2017-05-06 17:29:57 +0200

    Production version with external DB

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/GiveThatDevACookie',
+    // 'mongoUrl' : 'mongodb://localhost:27017/GiveThatDevACookie',
+    'mongoUrl' : 'mongodb://sarapulov:Sara7262@ds123930.mlab.com:23930/givethatdevacookie',
     'facebook': {
         clientID: 'XXX', // can be added if needed
         clientSecret: 'XXX', // can be added if needed
